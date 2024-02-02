8f02d3e7d1e8c73429e0a45e174e7fa68f0a7779
Arjun <arjun346002@gmail.com>
2024-02-02 12:33:57 +0530

    setup loader

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import { Provider } from "react-redux";
 import Body from "./components/Body";
 import appStore from "./utils/appStore";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+
   return (
-    <div>
-      <Provider store={appStore}>
-        <Body />
-      </Provider>
+    <div className="bg-black">
+      {loading ? (
+        <div className="flex items-center justify-center h-screen">
+          <div className="loader"></div>
+        </div>
+      ) : (
+        <Provider store={appStore}>
+          <Body />
+        </Provider>
+      )}
     </div>
   );
 }
