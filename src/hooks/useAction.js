31aa559f469b2538b28fccddfb702f5ec931a009
Arjun <arjun346002@gmail.com>
2024-02-03 11:26:25 +0530

    abc

--- src/hooks/useAction.js
+++ src/hooks/useAction.js
@@ -4,22 +4,38 @@ import { API_OPTIONS } from "../utils/constant";
 import { addActionMovies } from "../utils/movieSlice";
 import { useEffect } from "react";
 
-const useAction = ()=>{
-    const dispatch = useDispatch();
-    const actionMovies = useSelector(store=>store.movies.actionMovies);
+const useAction = () => {
+  const dispatch = useDispatch();
+  const actionMovies = useSelector((store) => store.movies.actionMovies);
 
-    const moviesList = async () => {
+  const moviesList = async () => {
+    try {
       const data = await fetch(
         "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1",
         API_OPTIONS
       );
+
+      if (!data.ok) {
+        throw new Error(`HTTP error! Status: ${data.status}`);
+      }
+
       const json = await data.json();
       dispatch(addActionMovies(json.results));
-    };
-  
-    useEffect(() => {
-     !actionMovies && moviesList();
-    }, []);
-}
+    } catch (error) {
+      console.error("Error fetching movies:", error);
+    }
+  };
+
+  useEffect(() => {
+    if (!actionMovies) {
+      moviesList();
+    }
+  }, [dispatch, actionMovies]);
+
+  return {
+    actionMovies,
+    fetchActionMovies: moviesList,
+  };
+};
 
 export default useAction;
