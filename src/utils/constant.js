fad3c3ee2e0ff7c25b1a38dfbd884789239a1136
Arjun <arjun346002@gmail.com>
2024-02-29 14:32:52 +0530

    changes

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,5 +1,5 @@
 export const LOGO_URL =
-  "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
+  "https://media.licdn.com/dms/image/C4D1BAQHi3FjNEaVSvQ/company-background_10000/0/1614596223769/movix_italia_cover?e=2147483647&v=beta&t=w_6de7VUtOWHJ2DXFljfFCtys1kCCS7TkNbCiwOsTDU";
 
 export const BODY_URL =
   "https://assets.nflxext.com/ffe/siteui/vlv3/c31c3123-3df7-4359-8b8c-475bd2d9925d/15feb590-3d73-45e9-9e4a-2eb334c83921/IN-en-20231225-popsignuptwoweeks-perspective_alpha_website_large.jpg";
